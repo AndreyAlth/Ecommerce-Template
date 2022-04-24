c21e39e3e6216f695e1f860772f2cfe28f0b52ea
AndreyAlth <andreyalth@gmail.com>
2022-04-23 22:00:31 -0500

    Agregando axios

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,3 +1,4 @@
+import { Axios } from 'axios';
 import React, {useState} from 'react';
 const axios = require('axios').default;
 
@@ -8,26 +9,64 @@ function Login() {
 
     async function loginUser() {
 
-        try {
-            const response = await axios.post('http://localhost:8080/api/auth/login', {
-                correo,
-                password
-            }).then((res) => {
-                console.log(res)
-            })
-            console.log(response);
+        
 
-            // const response = await fetch('http://localhost:8080/api/auth/login', {
-            //     method: "POST",
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     },
-            //     body: body
-            // })
-            console.log(response);
+        try {
+            const data = JSON.stringify({correo, password})
+            // const data = {correo, password}
+            console.log(data)
+
+            await axios({
+                method: 'POST',
+                url: 'http://localhost:8080/api/auth/login',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    "Access-Control-Allow-Origin": "*",
+                    'Accept':'*/*',
+                },
+                data
+              })
+              .then( (res) => console.log(res))
+              .then( data => {
+                  console.log(data);
+              })
+              .catch(error => {
+                console.log(error.response, error.message, error.request.response)
+            });
+            
         } catch (error) {
-          console.error(error);
         }
+
+
+        
+
+        
+        // const options = {
+        //     headers: {
+        //         'Content-type': 'text/plain'
+        //     }
+        // }
+        // try {
+        //     const response = await axios.post('http://localhost:8080/api/auth/login', {
+        //         correo: correo,
+        //         password: password
+        //     },{ options}).then((res) => {
+        //         console.log(res)
+        //     })
+        //     console.log(response);
+
+        //     // const response = await fetch('http://localhost:8080/api/auth/login', {
+        //     //     method: "POST",
+        //     //     headers: {
+        //     //         'Content-Type': 'application/json'
+        //     //     },
+        //     //     body: body
+        //     // })
+        //     console.log(response);
+        // } catch (error) {
+        //   console.error(error);
+        // }
       }
 
     return (
@@ -36,13 +75,13 @@ function Login() {
                 <div className="login">
                     <p className="login-title">Entrar</p>
                     <p className="login-text-1">Porfavor entrar con lo siguiente</p>
-                    {/* <input id='correo-input' className="login-text-input login-input" type="text" placeholder="Correo" onChange={ e => setCorreo(e.target.value)}/>
+                    <input id='correo-input' className="login-text-input login-input" type="text" placeholder="Correo" onChange={ e => setCorreo(e.target.value)}/>
                     <input id='password-input' className="login-text-input login-input" type="text" placeholder="Contraseña" onChange={ e => setPassword(e.target.value)}/>
                     <a href="" className="login-text-password">¿Olvidaste tu contraseña?</a>
                     <button 
                     className="login-button"
                     onClick={loginUser}
-                    >Entrar</button> */}
+                    >Entrar</button>
                     <hr className="division"/>
                     <a href="" className="login-text-password">Entrar con </a>
                     {/* <button className="login-button">Continuar con Facebook</button> */}
